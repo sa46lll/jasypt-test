a040024e0c5efa789cdda36e87840bfc62bb2cfd
sa46lll <9812smh@naver.com>
2022-11-14 16:57:28 +0900

    Initial commit

---
+++ src/main/java/com/example/jasypttest/JasyptTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jasypttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasyptTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasyptTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jasypttest/JasyptTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jasypttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
